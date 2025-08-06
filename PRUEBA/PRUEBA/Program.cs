d1906b4179feadd1581b3cc5032a3dc0a4b53594
kandrade <kevin_andrade7@outlook.com>
2025-08-06 15:40:56 -0500

    Tipo de datos

--- PRUEBA/PRUEBA/Program.cs
+++ PRUEBA/PRUEBA/Program.cs
@@ -17,6 +17,24 @@ namespace PRUEBA
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            int edad = 25;
+            double peso = 70.5;
+            float altura = 1.75f;
+            bool esMayor = true;
+            char inicial = 'K';
+            string nombre = "Kevin";
+
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Edad: " + edad);
+            Console.WriteLine("Peso: " + peso);
+            Console.WriteLine("Altura: " + altura);
+            Console.WriteLine("¿Es mayor de edad?: " + esMayor);
+            Console.WriteLine("Inicial: " + inicial);
+
+
         }
+
+
     }
 }
